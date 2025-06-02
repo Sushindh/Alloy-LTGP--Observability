38941eed4e824c8965f00c0b5a66e3a968c14a0a
Sushindh <146247085+Sushindh@users.noreply.github.com>
2025-06-02 10:28:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 import logging
 
 from opentelemetry import metrics, trace
-from opentelemetry.sdk.resources import Resource,SERVICE_NAME
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.sdk.trace import TracerProvider
@@ -14,78 +14,63 @@ from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.semconv.resource import ResourceAttributes
-#Name
-resource = Resource.create({ResourceAttributes.SERVICE_NAME:"flask-otel-app","deployment.environment":"dev","host.name":"flask-container","job": "flask-app"})
 
-#Initialize Opentelemetry SDK
+# Resource definition
+resource = Resource.create({
+    ResourceAttributes.SERVICE_NAME: "flask-otel-app",
+    "deployment.environment": "dev"
+})
 
-#Metrics
-
-# resource = Resource(attributes={SERVICE_NAME: "flask-app"})
-exporter = OTLPMetricExporter(endpoint="otel-collector:4317", insecure=True)
-reader = PeriodicExportingMetricReader(exporter)
-provider = MeterProvider(resource=resource, metric_readers=[reader])
-metrics.set_meter_provider(provider)
+# Metrics
+metric_exporter = OTLPMetricExporter(endpoint="alloy:4317", insecure=True)
+metric_reader = PeriodicExportingMetricReader(metric_exporter)
+metrics_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+metrics.set_meter_provider(metrics_provider)
 meter = metrics.get_meter(__name__)
-
-#Metrics instruments
 compute_request_count = meter.create_counter(
-    name='app_compute_request_count',
-    description='Counts the requests to compute-service',
-    unit='1'
+    name="app_compute_request_count",
+    description="Counts the requests",
+    unit="1"
 )
 
-#Tracing
+# Tracing
 trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer(__name__)
-span_exporter = OTLPSpanExporter(endpoint="otel-collector:4317", insecure=True)
-span_processor = BatchSpanProcessor(span_exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
+trace_exporter = OTLPSpanExporter(endpoint="alloy:4317", insecure=True)
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(trace_exporter))
 
-#Logs
+# Logging
 logger_provider = LoggerProvider(resource=resource)
-log_exporter = OTLPLogExporter(endpoint="otel-collector:4317",insecure=True)
-log_processor = BatchLogRecordProcessor(log_exporter)
-logger_provider.add_log_record_processor(log_processor)
-handler = LoggingHandler(level=logging.INFO, logger_provider=logger_provider)
-logging.basicConfig(level=logging.INFO, handlers=[handler])
-logger = logging.getLogger(__name__)
+log_exporter = OTLPLogExporter(endpoint="alloy:4317", insecure=True)
+logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
+logging_handler = LoggingHandler(level=logging.INFO, logger_provider=logger_provider)
+logging.basicConfig(level=logging.INFO, handlers=[logging_handler])
+logger = logging.getLogger("myapp")
 
+# Flask app
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 
-@app.route('/home')
+# Test log
+logger.info("Startup complete.")
+
+@app.route("/home")
 def home():
     compute_request_count.add(1, {"endpoint": "/home"})
-    logger.info("Home endpoint Hi", extra={
-    "service.name": "flask-otel-app",
-    "deployment.environment": "dev",
-    "host.name": "flask-container",
-    "job": "flask-app"
-})
+    logger.info("Home endpoint hit")
     return "Home"
 
-@app.route('/cart')
+@app.route("/cart")
 def cart():
-    compute_request_count.add(1, {"endpoint": "/cart"})
-    logger.info("Home endpoint Hi", extra={
-    "service.name": "flask-otel-app",
-    "deployment.environment": "dev",
-    "host.name": "flask-container",
-    "job": "flask-app"
-})
+    compute_request_count.add(2, {"endpoint": "/cart"})
+    logger.info("Cart endpoint hit")
     return "Cart"
 
-@app.route('/payment')
+@app.route("/payment")
 def payment():
-    compute_request_count.add(1, {"endpoint": "/payment"})
-    logger.info("Home endpoint Hi", extra={
-    "service.name": "flask-otel-app",
-    "deployment.environment": "dev",
-    "host.name": "flask-container",
-    "job": "flask-app"
-})
+    compute_request_count.add(3, {"endpoint": "/payment"})
+    logger.info("Payment endpoint hit")
     return "Payment"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
